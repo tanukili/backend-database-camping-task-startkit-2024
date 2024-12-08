6f690b289800b211bcd88eb32723b1f16d64dc3b
tanukile <backpacker320@gmail.com>
2024-12-09 01:18:31 +0800

    fix: 分批重傳 task6

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -181,16 +181,14 @@ values (
         -- 2. 預約時間`booking_at` 設為2024-11-24 16:00:00
         -- 3. 狀態`status` 設定為即將授課
 
-insert into "COURSE_BOOKING" (user_id,course_id, booking_at, created_at, status) values
+insert into "COURSE_BOOKING" (user_id,course_id, booking_at, status) values
 ((select id from "USER" where email = 'wXlTq@hexschooltest.io'),
 	(select id from "COURSE" where id = 1),
 	'2024-11-24 16:00:00',
-    '2024-11-24 16:00:00',
 	'status'),
 ((select id from "USER" where email = 'richman@hexschooltest.io'),
 	(select id from "COURSE" where id = 1),
 	'2024-11-24 16:00:00',
-    '2024-11-24 16:00:00',
 	'status');
 
 -- 5-2. 修改：`王小明`取消預約 `李燕容` 的課程，請在`COURSE_BOOKING`更新該筆預約資料：
@@ -288,14 +286,48 @@ inner join (
 -- 6-1 查詢：查詢專長為重訓的教練，並按經驗年數排序，由資深到資淺（需使用 inner join 與 order by 語法)
 -- 顯示須包含以下欄位： 教練名稱 , 經驗年數, 專長名稱
 
+select 
+	(select name from "USER" where id = c.user_id) as 教練名稱,
+	c.experience_years as 經驗年數,
+	(select name from "SKILL" s where name = '重訓') as 專長名稱
+from "COACH" c
+inner join "COACH_LINK_SKILL" cls on c.id = cls.coach_id
+where cls.skill_id = (select id from "SKILL" s where name = '重訓')
+order by experience_years desc;
+
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
+
+select 
+	s.name as 專長名稱,
+	count(*) as coach_total
+from "COACH_LINK_SKILL" cls
+inner join "SKILL" s on cls.skill_id = s.id
+group by s.id
+order by coach_total desc limit 1;
 
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
 
+select
+	(select name from "CREDIT_PACKAGE" cp2 where cp2.id = cp.credit_package_id) as 組合包方案名稱,
+	count(*) as 銷售數量 
+from "CREDIT_PURCHASE" cp
+group by credit_package_id;
+
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
 
+select
+	sum(price_paid) as 總營收
+from "CREDIT_PURCHASE"
+where purchase_at between '2024-11-01 00:00:00' and '2024-11-30 23:59:59';
+
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
+
+select
+    count(distinct user_id) as 預約會員人數
+from "COURSE_BOOKING"
+where created_at between '2024-11-01 00:00:00' and '2024-11-30 23:59:59' and
+    status not in ('cancelled');
